6f023a61d813cdc8c870df3743480d5ca9e1e19f
Luis <lva.luis15@gmail.com>
2017-11-24 10:11:38 -0500

    primera prueba

--- credits.py
+++ credits.py
@@ -6,7 +6,37 @@ texto_lista = '''
     "Cachimbo Bros"
 
     Con las participaciones de...
+Anthony Del Pozo (C) As Programador Master
+Jose Espinoza As Programador Junior/Diseñador
+Josue Palomino As Diseñador Master
+Luis Villanueva As Programador Junior/Diseñador
+Gustavo Reyes As "El hombre sin sombra"
 
+    Participaciones especiales de...
+
+    H. Quintana
+    F. Riccio
+    M. Campos
+    D. Cardenas
+    D. Llamas
+
+    Agradecimientos
+
+"La Cumbia de Pueblo Lavanda"
+"Pan con Pollo Doña Peta"
+"Manos S.A"
+"La tiendita de Don Pepe"
+"Codigo Facilito"
+
+    2017 © Copyright
+Todos los derechos Reservados
+
+
+    ¡¡ARRIBA PERÚ!!
+
+
+    ¡¡ERA HOY RAMON!!
+    ¡¡ERA HOY RAMON TE DIJEEE!!!
 
 '''.split('\n')
 
@@ -15,10 +45,10 @@ class Credits:
     def __init__(self, screen_rect, lst):
         self.srect = screen_rect
         self.lst = lst
-        self.size = 16
+        self.size = 20
         self.color = (255,255,255)
         self.buff_centery = self.srect.height/2 + 5
-        self.buff_lines = 50
+        self.buff_lines = 35
         self.timer = 0.0
         self.delay = 1
         self.hacer_surfaces()
@@ -56,6 +86,16 @@ class Credits:
 pantalla = pg.display.set_mode((800,600))
 rect_pantalla = pantalla.get_rect()
 clock = pg.time.Clock()
-hecho = False
+salir = False
 
 credi = Credits(rect_pantalla, texto_lista)
+
+while not salir:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            salir=True
+    pantalla.fill((0,0,0))
+    credi.update()
+    credi.materializar(pantalla)
+    pg.display.update()
+    clock.tick(60)
